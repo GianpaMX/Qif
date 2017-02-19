6f4a58508f25aaa0c166f75f052540d1eb251e72
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2017-02-18 23:22:13 -0600

    CLI

---
+++ CLI/CLI/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  CLI
+//
+//  Created by Juan Pablo Villa on 2/18/17.
+//  Copyright © 2017 gianpamx.github.io. All rights reserved.
+//
+
+import Foundation
+import Qif
+
+let qif = Qif()
+qif.setB(value: true)
+print(qif.getB())
+
